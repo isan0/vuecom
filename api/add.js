b4fdc9ec01ed69b96071bee7f1a763b2a50d620e
Ismael <ismael.aveiga11@gmail.com>
2022-02-10 18:34:04 +0100

    Guardar datos en BBDD

---
+++ api/add.js
@@ -0,0 +1,11 @@
+import { PSDB } from 'planetscale-node';
+const conn = new PSDB('main')
+  
+export default async function handler(req, res) {
+    const {
+        query: { contenido }
+    } = req
+
+    const [rows, fields] = await conn.query(`insert into comentarios(contenido) values ('${contenido}')`)
+    res.send(rows);
+}
--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <input v-model="subcomentario" placeholder="Deja tu comentario" @keypress.enter="guardarMensaje">
   <button @click="cargarComentarios">Cargar ya</button>
   <p v-for="comentario in comentarios" :key="comentario.id">
-    {{comentario.texto}}
+    {{comentario.contenido}}
   </p>
 </template>
 
@@ -13,17 +13,19 @@
       return {
         subcomentario: "holaaa",
         comentarios: [
-          {id: 1, texto: "adios muy buenas", fecha: "17/17/11"},
-          {id: 2, texto: "que tal muy buenas", fecha: "17/15/11"},
-          {id: 3, texto: "bye bye muy buenas", fecha: "17/16/11"},
-          {id: 4, texto: "jajaja muy buenas", fecha: "17/18/11"}
+          {id: 1, contenido: "adios muy buenas", fecha: "17/17/11"},
+          {id: 2, contenido: "que tal muy buenas", fecha: "17/15/11"},
+          {id: 3, contenido: "bye bye muy buenas", fecha: "17/16/11"},
+          {id: 4, contenido: "jajaja muy buenas", fecha: "17/18/11"}
         ]
       }
     },
     methods: {
       guardarMensaje(){
         // Aqui se guardara en la BBDD
-        this.comentarios.push({id: 5, texto: this.subcomentario})
+        let resultado = await (await fetch(`/api/add?contenido=${this.subcomentario}`));
+        this.comentarios.push({id: resultado.insertIs, contenido: this.subcomentario});
+        this.subcomentario="";
       },
       async cargarComentarios(){
         this.comentarios = await(await fetch("/api/get")).json();
