8881ce0251b5fa724ca861c75309a0b5e2e671e0
Ismael <ismael.aveiga11@gmail.com>
2022-02-24 17:51:05 +0100

    Projecto vue

--- api/get.js
+++ api/get.js
@@ -3,6 +3,5 @@ const conn = new PSDB('main')
   
 export default async function handler(req, res) {
     const [rows, fields] = await conn.query(`select * from comentarios`);
-
     res.send(rows);
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from 'vue-router'
+import {createAuth0} from '@auth0/auth0.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(router)
+    .use(createAuth0({
+        domain: "dev-frj0bk60.us.auth0.com",
+        client_id: "PoHfA3Z5i87zvUdlNwAnVZruOE5ErMWt",
+        redirect_uri: window.location.origin,
+        audience: "https://vuecom-project.vercel.app/api",
+        useRefreshTokens: true,
+        cacheLocation: 'localstorage'
+    }))
+    .mount('#app')
